bd5ecb2de9e3f3c863e3dc0f22503a53a8ff7fa4
apla <dot.and.thing@gmail.com>
2015-12-08 11:30:05 +0300

    more comments on test case

--- tests/integration/person.sql
+++ tests/integration/person.sql
@@ -5,7 +5,10 @@ CREATE TABLE person (
 	age DECIMAL(3),
 	gender CHAR(1),
 	expenses NUMERIC(10,2) default 0,
-	note TEXT,
+-- 	photo BLOB,	-- not supported by postgres
+-- 	note TEXT, -- not supported by oracle
 	birtday DATE,
+-- 	created_at DATETIME, -- not supported by pg
 	modified_at TIMESTAMP
-);
+-- 	modified_at TIMESTAMP(2) -- not supported by mssql
+)
